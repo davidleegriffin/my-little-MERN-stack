5e2f325e6f6730e5b231a744a17770823090c37c
= <davidleegriffin@yahoo.com>
2021-04-10 16:29:48 -0500

    initial

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 
+const db = require('./config/keys').mongoURI;
+const questions = require('./routes/api/questions');
+
+app.use(express.json());
+
+mongoose
+  .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log("mongo a go"))
+  .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send("this works!"))
+app.get('/', (req, res) => res.send("this works!"));
+app.use('/api/questions', questions);
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`server is serving on port ${port}`))
